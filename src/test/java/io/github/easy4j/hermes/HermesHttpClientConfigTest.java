00f45d8694481388bb693e51905fe52fa182db80
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 02:16:14 +0800

    refactor: expose chat scenario client

--- src/test/java/io/github/easy4j/hermes/HermesHttpClientConfigTest.java
+++ src/test/java/io/github/easy4j/hermes/HermesHttpClientConfigTest.java
@@ -1,6 +1,8 @@
 package io.github.easy4j.hermes;
 
 import org.junit.jupiter.api.Test;
+import io.github.easy4j.hermes.api.HermesChatClient;
+import io.github.easy4j.hermes.api.HermesHttpClient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,6 +12,8 @@ class HermesHttpClientConfigTest {
     void shouldExposeUnifiedStreamPropertiesAndLegacyAliases() {
         HermesHttpClientConfig config = new HermesHttpClientConfig();
         assertEquals(HttpResponseMode.BLOCKING, config.getMode());
+        config.setServerUrl("http://legacy-hermes");
+        assertEquals("http://legacy-hermes", config.getBaseUrl());
 
         config.setSseCorePoolSize(7);
         config.setSseMaxPoolSize(9);
@@ -22,5 +26,13 @@ class HermesHttpClientConfigTest {
         assertEquals(11, config.getStreamQueueCapacity());
         assertEquals(13L, config.getStreamKeepAliveMillis());
         assertEquals(15, config.getStreamEventQueueCapacity());
+    }
+
+    @Test
+    void shouldExposeChatClientAsHttpClientScenario() {
+        HermesHttpClientConfig config = new HermesHttpClientConfig();
+        try (HermesChatClient client = new HermesChatClient(config)) {
+            assertEquals(HermesHttpClient.class, client.getClass().getSuperclass());
+        }
     }
 }
